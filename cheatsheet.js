6fb042a2da9fa9c8cc9c8f826d67930580bf0981
Dan Weiner <daweiner16@gmail.com>
2015-12-16 11:16:48 -0500

    added initial html and js content

---
+++ cheatsheet.js
@@ -0,0 +1,9 @@
+var noun = prompt("Please enter a noun");
+console.log(noun);
+var adjective = prompt("Please enter an adjective");
+console.log(adjective);
+var verb = prompt("Please enter a verb");
+console.log(verb);
+var noun2 = prompt("Please enter another noun");
+console.log(noun2);
+alert("Thank you! Are you ready for your sentence?");
