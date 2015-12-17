f3885e00090376c8319f135ccb4282c38f9a3cf8
Dan Weiner <daweiner16@gmail.com>
2015-12-17 16:11:54 -0500

    added content to js and css files

--- cheatsheet.js
+++ cheatsheet.js
@@ -1,4 +1,12 @@
-// //Practice madlibs with variables and concatenation
+/****************
+Treehouse lessons used for practice
+*****************/
+
+
+/*******
+Practice madlibs with variables 
+and concatenation
+*******/
 
 // //Adding question countdown function
 
@@ -41,7 +49,9 @@
 
 
 
-// //Random Number Generator
+/*******
+Random Number Generator
+*******/
 
 // var input1 = prompt("Please type a starting number");
 // var bottomNumber = parseInt(input1);
@@ -59,12 +69,14 @@
 
 
 
-/*Random Number Guessing Game generates a number
+/*******
+Random Number Guessing Game generates a number
  between 1 and 6 and gives a player two chances to
-guess the number */
+guess the number 
+*******/
 
 
-// assumes guess was wrong
+//assumes guess was wrong
 
 // var correctGuess = false;
 
@@ -101,7 +113,9 @@ guess the number */
 
 
 
-// Quiz Challenge
+/*******
+Quiz Challenge
+*******/
 
 
 //quiz begins, no answers correct
@@ -152,30 +166,222 @@ guess the number */
 
 
 
-//Practice with Functions
+/*******
+Practice with Functions
+*******/
 
-//get random number between 1 and "upper"
-function getRandomNumber( upper ) {
-	var randomNumber = Math.floor(Math.random() * upper ) + 1;
-	return randomNumber;
-}
+//get random number between "lower" and "upper"
+// function getRandomNumber( lower, upper ) {
+// 	//tests if input is a number
+// 	if (isNaN(lower) || isNaN(upper)) {
+// 		//adds unique error message 
+// 		throw new Error('Both arguments must be numbers');
+// 	}
+// 	return Math.floor(Math.random() * (upper - lower + 1) ) + lower;
+// }
+
+// console.log(getRandomNumber(1, 6));
+// console.log(getRandomNumber("hundred", 200));
+// console.log(getRandomNumber(20, "house"));
+// console.log(getRandomNumber(30, 60));
+// console.log(getRandomNumber("eight", 80));
+
+
+
+// //calculate area of rectangle
+// function getArea(width, length, unit) {
+// 	var area = width * length;
+// 	return area + ' ' + unit;
+// }
+
+// console.log(getArea(10, 20, 'sq ft'));
+// document.write(getArea(5, 20, "sq ft"));
+
+
+
+
+
+
+
+/******
+Loops
+*******/
+
 /*
-console.log(getRandomNumber(6));
-console.log(getRandomNumber(100));
-console.log(getRandomNumber(200));
-console.log(getRandomNumber(300));
-console.log(getRandomNumber(400));
+While Loops
+*/
+
+//how many attempts it takes a computer to guess a number between 1 and 10000
+// var upper = 10000;
+// var randomNumber = getRandomNumber(upper);
+// var guess;
+// var attempts = 0;
+
+// function getRandomNumber(upper) {
+// 	return Math.floor( Math.random() * upper) + 1;
+// }
+
+// while ( guess !== randomNumber ) {
+// 	guess = getRandomNumber( upper );
+// 	attempts += 1;
+// }
+
+// document.write("<p>The random number was: " + randomNumber + "</p>");
+// document.write("<p>It took the computer " + attempts + " attempts to get it right.</p>");
+
+
+
+/*Making the do...while loop below
+into a while loop using "break"*/
+
+// var randomNumber = getRandomNumber(10);
+// var guess;
+// var guessCount = 0;
+// var correctGuess = false;
+
+// function getRandomNumber( upper ) {
+//   var num = Math.floor(Math.random() * upper) + 1; 
+//   return num;
+// }
+
+// while (true) {
+// 	guess = prompt("I'm thinking of a number between 1 and 10.  What is it?");
+// 	guessCount += 1;
+// 	if (parseInt(guess) === randomNumber) {
+// 		correctGuess = true;
+// 		break;
+// 	}
+// }
+// 	document.write("It took you " + guessCount + " tries to guess the number " + randomNumber);
+
+
+/*
+Do ... While Loops
+*/
+
+// var randomNumber = getRandomNumber(10);
+// var guess;
+// var guessCount = 0;
+// var correctGuess = false;
+
+// function getRandomNumber( upper ) {
+//   var num = Math.floor(Math.random() * upper) + 1; 
+//   return num;
+// }
+
+// do {
+// 	guess = prompt("I'm thinking of a number between 1 and 10.  What is it?");
+// 	guessCount += 1;
+// 	if (parseInt(guess) === randomNumber) {
+// 		correctGuess = true;
+// 	}
+// }	while ( ! correctGuess )
+// 	document.write("It took you " + guessCount + " tries to guess the number " + randomNumber);
+
+
+
+/* 
+For Loops 
+*/
+
+/*added circle shaped "divs" to webpage with numbers
+1 through 10*/
+// var html = '';
+// for (var i = 1; i <= 10; i += 1) {
+// 	html += "<div>" + i + "</div>";
+// }
+// document.write(html);
+
+
+
+/*
+Refactor challenge using loops
 */
 
 
-//calculate area of rectangle
-function getArea(width, length, unit) {
-	var area = width * length;
-	return area + ' ' + unit;
+//generates ten random colored circles
+
+// var html = '';
+// var rgbColor;
+
+// function randomRGB() {
+// 	return Math.floor(Math.random() * 256);
+// }
+
+// function randomColor() {
+// 	var color = "rgb(";
+// 	color += randomRGB() + ",";
+// 	color += randomRGB() + ",";
+// 	color += randomRGB() + ")";
+// 	return color;
+// }
+
+// function print(message) {
+// 	document.write(message);
+// }
+
+// for (var i = 1; i <= 10; i += 1) {
+// 	rgbColor = randomColor();
+// 	html += '<div style="background-color:' + rgbColor + '"></div>';
+// }
+// print(html);
+
+
+
+
+
+
+/******
+Arrays
+*******/
+
+//components for the character "冶"
+
+// var 冶 = ["冫", "厶", "口"];
+
+// document.write(冶[1]);
+
+// console.log(冶[1])
+
+
+//adding items to an array
+
+
+
+function printList( list ) {
+  var listHTML = '<ol>';
+  for (var i = 0; i < list.length; i += 1) {
+    listHTML += '<li>' + list[i] + '</li>';
+  }
+  listHTML += '</ol>';
+  print(listHTML);
 }
 
-console.log(getArea(10, 20, 'sq ft'));
-document.write(getArea(5, 20, "sq ft"));
+function print(html) {
+  document.write(html);
+}
+
+var 冶 = [];
+冶.push("厶", "口");
+冶.unshift("冫");
+
+printList (冶);
+
+var 総 = ["糸", "公"];
+総.push("心");
+console.log(総);
+
+
+
+/******
+Objects
+*******/
+
+// var 冶1 = {jlpt: "N3", grade: "3", components: ["冫", "厶", "口"]};
+
+// document.write(冶1.components[0]);
+
+// console.log(冶1.components[0]);
 
 
 
